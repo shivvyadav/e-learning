c8950f172a076932734061076ca48bb6e094b989
Shiv  Yadav <shivyadav0014@gmail.com>
2026-02-22 16:45:06 +0545

    update the main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import Router from "./Router.jsx";
 
-import './index.css';
-import { AdminProvider } from './context/AdminContext';
-
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AdminProvider>
-      <App />
-    </AdminProvider>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router />
+  </StrictMode>,
 );
-
