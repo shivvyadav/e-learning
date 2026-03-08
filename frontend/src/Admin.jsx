51cd7fb442a33fd1a5c2ed1ed9283ad865bbdba1
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-08 17:05:52 +0545

    add the dashboard page

--- frontend/src/Admin.jsx
+++ frontend/src/Admin.jsx
@@ -1,5 +1,56 @@
 import React from "react";
+import {Users, UserCheck, BookOpen, Star} from "lucide-react";
 
 export default function Admin() {
-  return <div>Admin</div>;
+  const stats = [
+    {
+      title: "Total Users",
+      value: "1,164",
+      icon: <Users size={20} className='text-white' />,
+      bg: "bg-orange-500",
+    },
+    {
+      title: "Total Enrolled Users",
+      value: "834",
+      icon: <UserCheck size={20} className='text-white' />,
+      bg: "bg-blue-500",
+    },
+    {
+      title: "Total Courses",
+      value: "31",
+      icon: <BookOpen size={20} className='text-white' />,
+      bg: "bg-purple-500",
+    },
+    {
+      title: "Total Reviews",
+      value: "156",
+      icon: <Star size={20} className='text-white' />,
+      bg: "bg-teal-500",
+    },
+  ];
+
+  return (
+    <div className='w-full py-6 '>
+      {/* Cards Container */}
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+        {stats.map((item, index) => (
+          <div
+            key={index}
+            className='flex items-center justify-between bg-white rounded-xl shadow-sm p-6'>
+            {/* Left */}
+            <div className='flex items-center gap-4'>
+              <div className={`w-12 h-12 flex items-center justify-center rounded-full ${item.bg}`}>
+                {item.icon}
+              </div>
+
+              <div>
+                <p className='text-gray-500 text-sm'>{item.title}</p>
+                <p className='text-xl font-semibold'>{item.value}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
