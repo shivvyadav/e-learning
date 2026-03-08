cf25bf7dad41d7430dc41eba0a26cdecbd377270
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-08 17:28:23 +0545

    edit the admin layout

--- frontend/src/Admin.jsx
+++ frontend/src/Admin.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import {Users, UserCheck, BookOpen, Star} from "lucide-react";
 
+import NewUsers from "./components/NewUsers";
+import LatestReviews from "./components/LatestReviews";
+import LatestPurchases from "./components/LatestPurchases";
+import TopCourses from "./components/TopCourses";
+
 export default function Admin() {
   const stats = [
     {
@@ -30,24 +35,35 @@ export default function Admin() {
   ];
 
   return (
-    <div className='w-full py-6 '>
+    <div className='p-4 md:p-6 space-y-6'>
+      {/* Stats */}
       <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
         {stats.map((item, index) => (
           <div
             key={index}
-            className='flex items-center justify-between bg-neutral-50 rounded-xl shadow-sm p-6'>
-            <div className='flex items-center gap-4'>
-              <div className={`w-12 h-12 flex items-center justify-center rounded-full ${item.bg}`}>
-                {item.icon}
-              </div>
+            className='flex items-center gap-4 bg-neutral-50 border border-neutral-300 rounded-xl p-6'>
+            <div className={`w-12 h-12 flex items-center justify-center rounded-full ${item.bg}`}>
+              {item.icon}
+            </div>
 
-              <div>
-                <p className='text-gray-500 text-sm'>{item.title}</p>
-                <p className='text-xl font-semibold'>{item.value}</p>
-              </div>
+            <div>
+              <p className='text-sm text-gray-500'>{item.title}</p>
+              <p className='text-xl font-semibold text-gray-800'>{item.value}</p>
             </div>
           </div>
         ))}
+      </div>
+
+      {/* Section 1 */}
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <NewUsers />
+        <LatestReviews />
+      </div>
+
+      {/* Section 2 */}
+      <div className='grid gap-6 lg:grid-cols-2'>
+        <LatestPurchases />
+        <TopCourses />
       </div>
     </div>
   );
