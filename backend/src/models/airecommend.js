99175d768a600d5409d0f74db38248228e8e9915
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-19 22:34:48 +0545

    add the ai recommend model file

---
+++ backend/src/models/airecommend.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+
+const RecommendationSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  recommendedCourses: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Course"
+    }
+  ],
+  generatedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const airecommend= mongoose.model("airecommend",RecommendationSchema)
+module.exports = airecommend
