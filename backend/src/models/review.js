24940a75f0e90f10c71072ed691b39a3285e5cc3
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-19 22:32:56 +0545

    add the review model

---
+++ backend/src/models/review.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const ReviewSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  courseId: {
+    type:Schema.Types.ObjectId,
+    ref: "course"
+  },
+  rating: Number,
+  comment: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+const review = mongoose.model("review",ReviewSchema)
+module.exports = review
