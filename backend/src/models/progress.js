804b914944a545312dc35d8e912349f20698c600
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-19 22:33:09 +0545

    add the progress model

---
+++ backend/src/models/progress.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+
+const ProgressSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: "course"
+  },
+  completedvideos:[
+            {
+                _id: { type: Schema.Types.ObjectId, auto:true },
+                title:String,
+                videoUrl:String,
+                duration:Number
+            }
+        ],
+  completedpdfs:[
+            {
+                _id:{ type:Schema.Types.ObjectId, auto:true },
+                title:String,
+                pdfUrl:String
+            }
+        ],
+  lastAccessed: Date
+})
+
+const progress = mongoose.model("progress",ProgressSchema)
+module.exports = progress
