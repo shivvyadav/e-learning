a5081a92f2bc944a3350824138bc5ccf10b0446f
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-19 22:33:48 +0545

    add the course related model

---
+++ backend/src/models/courserelated.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const paymentSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,ref:"User"
+    },
+    course:{
+         type:Schema.Types.ObjectId,ref:"course"
+    },
+    totalamount:{type:Number , required:true},
+    phonenumber:{type:Number,required:true},
+
+    paymentdetail:{
+        pidx:{type:String},
+        method:{type:String,default:"khalti"},
+        status:{type:String,eum:['paid','unpaid','pending'],default:'pending'}
+
+    }
+
+},{
+    timestamps:true
+})
+
+const courseselect = mongoose.model("courseselect",paymentSchema)
+module.exports = courseselect
