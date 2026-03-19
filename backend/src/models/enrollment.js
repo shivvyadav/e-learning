2ae565368b063fedc0b02d17ebbde8bcc983b30c
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-19 22:33:28 +0545

    add the enrollment model

---
+++ backend/src/models/enrollment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const enrollmentSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,ref:"User"
+    },
+    course:{
+        type:Schema.Types.ObjectId,ref:"course"
+    },
+    enrolleddate:{
+        type:Date,
+        default:Date.now
+    },
+    status:{
+        type:String,
+        enum:["active","completed"],
+        default:"active"
+
+    },
+    progress:{
+        type:Number,
+        default:0
+    }
+})
+
+const enroll = mongoose.model("enroll",enrollmentSchema)
+module.exports = enroll
