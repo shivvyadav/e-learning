5bb920d62cb9a1a12b1462b84266064fc0c024f6
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-23 16:37:48 +0545

    add the context api in the main page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import Router from "./Router.jsx";
+import {AdminProvider} from "./context/AdminContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <AdminProvider>
+      <Router />
+    </AdminProvider>
   </StrictMode>,
 );
