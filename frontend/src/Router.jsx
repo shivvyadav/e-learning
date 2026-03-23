bb539f2b219f12e78347074178a9d2cf7f3a96bf
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-23 16:38:24 +0545

    add the context api and protected the routes

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,27 +1,28 @@
 import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import {useState} from "react";
-import Admin from "./Admin.jsx";
+
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
-import Login from "./pages/Login.jsx";
 
+import Login from "./pages/Login.jsx";
+import Admin from "./pages/Admin.jsx";
 import Users from "./pages/Users.jsx";
 import Courses from "./pages/Courses.jsx";
 import Purchases from "./pages/Purchases.jsx";
 import Reviews from "./pages/Reviews.jsx";
 
+import ProtectedRoute from "./context/ProtectedRoute.jsx";
+
 const Layout = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='h-screen overflow-hidden flex'>
+    <div className='h-screen flex'>
       <Navbar setIsOpen={setIsOpen} />
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-      <div className='flex-1 flex flex-col pt-20 lg:ml-64'>
-        <div className='flex-1 overflow-y-auto px-4'>
-          <Outlet />
-        </div>
+      <div className='flex-1 pt-20 lg:ml-64'>
+        <Outlet />
       </div>
     </div>
   );
@@ -30,7 +31,11 @@ const Layout = () => {
 const Router = () => {
   const router = createBrowserRouter([
     {
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {path: "/", element: <Admin />},
         {path: "/courses", element: <Courses />},
