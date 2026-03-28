00271a864424f6a1a924dda4a6c690dadeec27d9
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-28 22:08:47 +0545

    update the payment route

--- backend/src/routes/user/paymentroute.js
+++ backend/src/routes/user/paymentroute.js
@@ -1,12 +1,13 @@
-const { intitialkhaltipayment, verifypdx } = require("../../controllers/user/payment/paymentcontroller")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const catchasync = require("../../services/catchasync")
+const {
+  intitialkhaltipayment,
+  verifypdx,
+} = require("../../controllers/user/payment/paymentcontroller");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const catchasync = require("../../services/catchasync");
 
+const router = require("express").Router();
 
+router.route("/payment").post(isAuthenticated, catchasync(intitialkhaltipayment));
+router.route("/verify").post(isAuthenticated, catchasync(verifypdx));
 
-const router = require("express").Router()
-
-router.route("/payment").post(isAuthenticated,catchasync(intitialkhaltipayment))
-router.route("/verify").post(isAuthenticated,catchasync(verifypdx))
-
-module.exports = router
+module.exports = router;
