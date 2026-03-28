2624a177e62171ebd338003489cd0ac5d7f4bcde
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-28 22:09:35 +0545

    update the course select route

--- backend/src/routes/user/courseselectroute.js
+++ backend/src/routes/user/courseselectroute.js
@@ -1,15 +1,22 @@
-const { createcourseselection, getmyselectedcourse, updatemycourseselection, deletemycourseselection } = require("../../controllers/user/courseselection/courseselectcontroller")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const catchasync = require("../../services/catchasync")
+const {
+  createcourseselection,
+  getmyselectedcourse,
+  updatemycourseselection,
+  deletemycourseselection,
+} = require("../../controllers/user/courseselection/courseselectcontroller");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const catchasync = require("../../services/catchasync");
 
-const router = require("express").Router()
+const router = require("express").Router();
 
-router.route("/courseSelect")
-.post(isAuthenticated,catchasync(createcourseselection))
-.get(isAuthenticated,catchasync(getmyselectedcourse))
+router
+  .route("/courseSelect")
+  .post(isAuthenticated, catchasync(createcourseselection))
+  .get(isAuthenticated, catchasync(getmyselectedcourse));
 
-router.route("/courseSelect/:id")
-.patch(isAuthenticated,catchasync(updatemycourseselection))
-.delete(isAuthenticated,catchasync(deletemycourseselection))
+router
+  .route("/courseSelect/:id")
+  .patch(isAuthenticated, catchasync(updatemycourseselection))
+  .delete(isAuthenticated, catchasync(deletemycourseselection));
 
-module.exports = router
+module.exports = router;
