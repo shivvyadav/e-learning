4a450326f486980a5781266cb8a17da5807d575c
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-28 22:06:46 +0545

    ass the payment route

---
+++ backend/src/routes/user/paymentroute.js
@@ -0,0 +1,12 @@
+const { intitialkhaltipayment, verifypdx } = require("../../controllers/user/payment/paymentcontroller")
+const isAuthenticated = require("../../middlewares/authMiddleware")
+const catchasync = require("../../services/catchasync")
+
+
+
+const router = require("express").Router()
+
+router.route("/payment").post(isAuthenticated,catchasync(intitialkhaltipayment))
+router.route("/verify").post(isAuthenticated,catchasync(verifypdx))
+
+module.exports = router
