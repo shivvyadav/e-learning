86af1405535d7c86aeb6303fe342347f48567c54
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:17:45 +0545

    update the review model file

--- backend/src/models/review.js
+++ backend/src/models/review.js
@@ -1,24 +1,22 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   courseId: {
-    type:Schema.Types.ObjectId,
-    ref: "course"
+    type: Schema.Types.ObjectId,
+    ref: "course",
   },
   rating: Number,
   comment: String,
   createdAt: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-
-const review = mongoose.model("review",ReviewSchema)
-module.exports = review
+const review = mongoose.model("review", ReviewSchema);
+module.exports = review;
