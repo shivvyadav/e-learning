e201dc9160646e684df1f8882650b02f64b55bdc
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:18:06 +0545

    update the user model file

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,48 +1,47 @@
-const mongoose =require('mongoose')
-const Schema= mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    useremail:{
-        type:String,
-        required :[true,'Email Must be provided'],
-        unique:true,
-        lowercase:true
+    useremail: {
+      type: String,
+      required: [true, "Email Must be provided"],
+      unique: true,
+      lowercase: true,
     },
-    userphonenumber:{
-        type:Number,
-        required:[true,"phonenumber must be provided"]
+    userphonenumber: {
+      type: Number,
+      required: [true, "phonenumber must be provided"],
     },
-    username:{
-      type:String,
-      required:[true,"username must be provided"]
+    username: {
+      type: String,
+      required: [true, "username must be provided"],
     },
-    userpassword:{
-      type:String,
-      required:[true],
-      select:false //this will not be shown  in the frontend 
+    userpassword: {
+      type: String,
+      required: [true],
+      select: false,
     },
-    profileimage:{
-      type:String
+    profileimage: {
+      type: String,
     },
-    role:{
-        type:String,
-        enum:["customer","admin"],
-        default:"customer"
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
     },
-    otp:{
-      type:Number
+    otp: {
+      type: Number,
     },
-    isotpverified:{
-      type:Boolean,
-      default:false
+    isotpverified: {
+      type: Boolean,
+      default: false,
     },
-    
   },
   {
     timestamps: true,
   },
 );
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 module.exports = User;
