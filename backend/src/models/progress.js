f97268170b1c89e1fdf03e39fd8f6c7c7ea21efa
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:17:23 +0545

    update the progress model file

--- backend/src/models/progress.js
+++ backend/src/models/progress.js
@@ -1,34 +1,32 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ProgressSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   course: {
     type: Schema.Types.ObjectId,
-    ref: "course"
+    ref: "course",
   },
-  completedvideos:[
-            {
-                _id: { type: Schema.Types.ObjectId, auto:true },
-                title:String,
-                videoUrl:String,
-                duration:Number
-            }
-        ],
-  completedpdfs:[
-            {
-                _id:{ type:Schema.Types.ObjectId, auto:true },
-                title:String,
-                pdfUrl:String
-            }
-        ],
-  lastAccessed: Date
-})
+  completedvideos: [
+    {
+      _id: {type: Schema.Types.ObjectId, auto: true},
+      title: String,
+      videoUrl: String,
+      duration: Number,
+    },
+  ],
+  completedpdfs: [
+    {
+      _id: {type: Schema.Types.ObjectId, auto: true},
+      title: String,
+      pdfUrl: String,
+    },
+  ],
+  lastAccessed: Date,
+});
 
-const progress = mongoose.model("progress",ProgressSchema)
-module.exports = progress
+const progress = mongoose.model("progress", ProgressSchema);
+module.exports = progress;
