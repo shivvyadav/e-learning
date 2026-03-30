4fc53bd5efdc5afa69b4e406a2c2b63ce53a34df
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:13:41 +0545

    add the multer configuration

---
+++ backend/src/middlewares/multerconfig.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // check the mimetypes of file
+    const allowfiletypes = [
+      "image/png",
+      "image/jpg",
+      "image/JPG",
+      "image/jpeg",
+      "image/JPEG",
+      "application/pdf",
+      "video/mp4",
+    ];
+
+    if (!allowfiletypes.includes(file.mimetype)) {
+      return cb(new Error("This file type is not supported"));
+    }
+
+    cb(null, "./uploads");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+module.exports = {multer, storage};
