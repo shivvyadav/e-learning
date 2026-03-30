edca84607b4046ba0996fe653f595b603eea2561
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:14:05 +0545

    update the multer configuration file

--- backend/src/middlewares/multerconfig.js
+++ backend/src/middlewares/multerconfig.js
@@ -2,7 +2,6 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // check the mimetypes of file
     const allowfiletypes = [
       "image/png",
       "image/jpg",
