2f58d4c589dde96518b7e4fd002f96a6cd4e7bf9
Shiv  Yadav <shivyadav0014@gmail.com>
2026-03-30 23:17:02 +0545

    update the enrollment model file

--- backend/src/models/enrollment.js
+++ backend/src/models/enrollment.js
@@ -1,28 +1,29 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const enrollmentSchema = new Schema({
-    user:{
-        type:Schema.Types.ObjectId,ref:"User"
-    },
-    course:{
-        type:Schema.Types.ObjectId,ref:"course"
-    },
-    enrolleddate:{
-        type:Date,
-        default:Date.now
-    },
-    status:{
-        type:String,
-        enum:["active","completed"],
-        default:"active"
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: "course",
+  },
+  enrolleddate: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ["active", "completed"],
+    default: "active",
+  },
+  progress: {
+    type: Number,
+    default: 0,
+  },
+});
 
-    },
-    progress:{
-        type:Number,
-        default:0
-    }
-})
-
-const enroll = mongoose.model("enroll",enrollmentSchema)
-module.exports = enroll
+const enroll = mongoose.model("enroll", enrollmentSchema);
+module.exports = enroll;
