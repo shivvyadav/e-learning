e44dafdf04a367c4fe3a151d7488b6a77c5c46fc
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-02 21:35:25 +0545

    add the enroll controller file

---
+++ backend/src/controllers/admin/enroll/enrollementcontroller.js
@@ -0,0 +1,16 @@
+const enroll = require("../../../models/enrollment");
+
+
+exports.getAllEnrollments = async (req, res) => {
+   
+    const enrollments = await enroll.find()
+      .populate("user")
+      .populate("course");
+
+    res.status(200).json({
+        message:"enrollment fetched successfully",
+        data:enrollments
+    });
+}
+  
+    
