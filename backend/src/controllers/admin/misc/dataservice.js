d74539c8ad88b7678fc7b6408d6ad42cdab09edc
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-02 21:39:08 +0545

    update the misc data services file

--- backend/src/controllers/admin/misc/dataservice.js
+++ backend/src/controllers/admin/misc/dataservice.js
@@ -5,13 +5,6 @@ const User = require("../../../models/User");
 const enrollment = require("../../../models/enrollment");
 
 exports.getdata = async (req, res) => {
-  // const courses = (await course.find()).length
-  // const users = (await User.find()).length
-  // const reviews = await review.find()
-  // const reviewlength =  reviews.length
-  // const allcourseselect = await courseselect.find().populate('user').populate('course')
-  // const courseselected = allcourseselect.length
-
   const courses = await course.find();
   const users = await User.find();
   const reviews = await review.find().populate("userId").populate("courseId");
@@ -20,12 +13,6 @@ exports.getdata = async (req, res) => {
   res.status(200).json({
     message: "data fetched successfully",
     data: {
-      //   courses,
-      //   users,
-      //   courseselected,
-      //   allcourseselect,
-      //   reviews,
-      // reviewlength
       courses,
       users,
       enrollments,
