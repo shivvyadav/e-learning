e6d14be1d05d8dff82ab21218b004717169b3e60
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-02 21:38:33 +0545

    update enrollment related controller file

--- backend/src/controllers/admin/enroll/enrollementcontroller.js
+++ backend/src/controllers/admin/enroll/enrollementcontroller.js
@@ -1,16 +1,10 @@
 const enroll = require("../../../models/enrollment");
 
-
 exports.getAllEnrollments = async (req, res) => {
-   
-    const enrollments = await enroll.find()
-      .populate("user")
-      .populate("course");
+  const enrollments = await enroll.find().populate("user").populate("course");
 
-    res.status(200).json({
-        message:"enrollment fetched successfully",
-        data:enrollments
-    });
-}
-  
-    
+  res.status(200).json({
+    message: "enrollment fetched successfully",
+    data: enrollments,
+  });
+};
