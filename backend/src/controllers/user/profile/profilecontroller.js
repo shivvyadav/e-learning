1e9d8b134561bc43b7891e21803b50ecfac01d6e
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-03 22:29:19 +0545

    update the profile controller

--- backend/src/controllers/user/profile/profilecontroller.js
+++ backend/src/controllers/user/profile/profilecontroller.js
@@ -1,71 +1,67 @@
-// const User = require("../../../models/User")
-const User = require("../../../models/User")
-const bcrypt = require("bcryptjs")
+const User = require("../../../models/User");
+const bcrypt = require("bcryptjs");
 
+exports.getmyprofile = async (req, res) => {
+  const userId = req.user.id;
+  const myprofile = await User.findById(userId);
+  res.status(200).json({
+    data: myprofile,
+    message: "profile fetched successfully",
+  });
+};
 
-exports.getmyprofile = async(req,res)=>{
-    const userId = req.user.id 
-    const myprofile = await User.findById(userId)
-    res.status(200).json({
-        data:myprofile,
-        message:"profile fetched successfully"
-    })
-}
+exports.updatemyprofile = async (req, res) => {
+  const {username, useremail, userphonenumber} = req.body;
+  const userId = req.user.id;
+  const updatedata = await User.findByIdAndUpdate(
+    userId,
+    {username, useremail, userphonenumber},
+    {
+      runValidators: true,
+      new: true,
+    },
+  );
+  res.status(200).json({
+    message: "profile  updated successfully",
+    data: updatedata,
+  });
+};
 
-exports.updatemyprofile = async(req,res)=>{
-    const {username,useremail,userphonenumber}= req.body 
-    const userId = req.user.id 
-    const updatedata = await User.findByIdAndUpdate(userId,{username,useremail,userphonenumber},{
-        runValidators:true,
-        new:true
-    })
-    res.status(200).json({
-        message:"profile  updated successfully",
-        data:updatedata
-    })
-}
+exports.deletemyprofile = async (req, res) => {
+  const userId = req.user.id;
+  await User.findByIdAndDelete(userId);
+  res.status(200).json({
+    message: "profile deleted successfully",
+    data: null,
+  });
+};
 
-exports.deletemyprofile = async(req,res)=>{
-    const userId = req.user.id 
-    await User.findByIdAndDelete(userId)
-    res.status(200).json({
-       message:"profile deleted successfully",
-       data:null
-    })
-}
+exports.updatemypass = async (req, res) => {
+  const userId = req.user.id;
+  const {oldpassword, newpassword, confirmpassword} = req.body;
+  if (!oldpassword || !newpassword || !confirmpassword) {
+    return res.status(400).json({
+      message: "please provide oldpassword,newpassword,confirmpassword",
+    });
+  }
+  if (newpassword !== confirmpassword) {
+    return res.staust(400).json({
+      message: "newpassword and confirmpassowrd didnot matched",
+    });
+  }
+  const userdata = await User.findById(userId);
+  const hasholdpassword = userdata.userpassword;
 
-exports.updatemypass=async(req,res)=>{
-    const userId =req.user.id
-    const{oldpassword,newpassword,confirmpassword} =req.body 
-    if(!oldpassword || !newpassword ||!confirmpassword){
-        return res.status(400).json({
-            message:"please provide oldpassword,newpassword,confirmpassword"
-        })
-    }
-    if(newpassword !==confirmpassword){
-        return res.staust(400).json({
-            message:"newpassword and confirmpassowrd didnot matched"
-        })
-    }
-    //taking out the hash of the old password 
-    const userdata=await User.findById(userId)
-    const hasholdpassword=userdata.userpassword
+  const isoldpasswordcorrect = await bcrypt.compareSync(oldpassword, hasholdpassword);
+  if (!isoldpasswordcorrect) {
+    return res.status(400).json({
+      message: "oldpassword didnot matched",
+    });
+  }
 
-    //check if oldpassword is correct ot not
-    const isoldpasswordcorrect=await bcrypt.compareSync(oldpassword,hasholdpassword)
-    if(!isoldpasswordcorrect){
-        return res.status(400).json({
-            message:"oldpassword didnot matched"
-        })
-    }
-
-    //matched vayo vane
-    userdata.userpassword=bcrypt.hashSync(newpassword,12)
-    await userdata.save()
-    res.status(200).json({
-        message:"password changed successfully"
-    })
-}
-
-
-
+  userdata.userpassword = bcrypt.hashSync(newpassword, 12);
+  await userdata.save();
+  res.status(200).json({
+    message: "password changed successfully",
+  });
+};
