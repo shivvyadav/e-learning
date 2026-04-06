223d83382fec147c6fdeb24b23edcd7e52434a3a
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:53:00 +0545

    add the admin course select route

--- backend/src/routes/admin/admincourseselectroute.js
+++ backend/src/routes/admin/admincourseselectroute.js
@@ -1,15 +1,23 @@
+const {
+  getallselectedcourse,
+  updatePaymentStatus,
+  getsingleselectedcourse,
+  deleteselectedcourse,
+} = require("../../controllers/admin/courserelated/courseselectcontroller");
+const isauthenticated = require("../../middlewares/authMiddleware");
+const restrictto = require("../../middlewares/restrict");
+const catchasync = require("../../services/catchasync");
 
-const { getallselectedcourse, updatePaymentStatus, getsingleselectedcourse, deleteselectedcourse } = require("../../controllers/admin/courserelated/courseselectcontroller")
-const isauthenticated = require("../../middlewares/authMiddleware")
-const restrictto = require("../../middlewares/restrict")
-const catchasync = require("../../services/catchasync")
+const router = require("express").Router();
+router
+  .route("/admincourseselect")
+  .get(isauthenticated, restrictto("admin"), catchasync(getallselectedcourse));
+router
+  .route("/courseselect/paymentstatus/:id")
+  .patch(isauthenticated, restrictto("admin"), catchasync(updatePaymentStatus));
+router
+  .route("/admincourseselect/:id")
+  .get(isauthenticated, restrictto("admin"), catchasync(getsingleselectedcourse))
+  .delete(isauthenticated, restrictto("admin"), catchasync(deleteselectedcourse));
 
-const router = require("express").Router()
- router.route("/admincourseselect").get(isauthenticated,restrictto("admin"),catchasync(getallselectedcourse))
- router.route("/courseselect/paymentstatus/:id").patch(isauthenticated,restrictto("admin"),catchasync(updatePaymentStatus))
- router.route("/admincourseselect/:id").get(isauthenticated,restrictto("admin"),catchasync(getsingleselectedcourse))
- .delete(isauthenticated,restrictto("admin"),catchasync(deleteselectedcourse))
-
-
-
-module.exports=router
+module.exports = router;
