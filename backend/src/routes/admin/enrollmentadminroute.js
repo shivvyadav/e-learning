9a82b467ce8bea454913a2a26c979c91f248c50a
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:54:25 +0545

    add the enrollment admin route

--- backend/src/routes/admin/enrollmentadminroute.js
+++ backend/src/routes/admin/enrollmentadminroute.js
@@ -1,14 +1,9 @@
+const {getAllEnrollments} = require("../../controllers/admin/enroll/enrollementcontroller");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const restrictto = require("../../middlewares/restrict");
+const catchasync = require("../../services/catchasync");
 
+const router = require("express").Router();
+router.route("/enroll").get(isAuthenticated, restrictto("admin"), catchasync(getAllEnrollments));
 
-
-const { getAllEnrollments } = require("../../controllers/admin/enroll/enrollementcontroller")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const restrictto = require("../../middlewares/restrict")
-const catchasync = require("../../services/catchasync")
-
-
-
-const router = require("express").Router()
-router.route("/enroll").get(isAuthenticated,restrictto("admin"),catchasync(getAllEnrollments))
-
-module.exports =router
+module.exports = router;
