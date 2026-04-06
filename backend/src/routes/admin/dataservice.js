b646a5814cabaf0efa8017ee1166a88ebf43cc80
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:54:02 +0545

    add the data service route

--- backend/src/routes/admin/dataservice.js
+++ backend/src/routes/admin/dataservice.js
@@ -1,11 +1,10 @@
-const { getdata } = require("../../controllers/admin/misc/dataservice")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const restrictto = require("../../middlewares/restrict")
-const catchasync = require("../../services/catchasync")
+const {getdata} = require("../../controllers/admin/misc/dataservice");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const restrictto = require("../../middlewares/restrict");
+const catchasync = require("../../services/catchasync");
 
+const router = require("express").Router();
 
-const router =   require("express").Router()
+router.route("/misc/datas").get(isAuthenticated, restrictto("admin"), catchasync(getdata));
 
-router.route("/misc/datas").get(isAuthenticated,restrictto("admin"),catchasync(getdata))
-
-module.exports =router
+module.exports = router;
