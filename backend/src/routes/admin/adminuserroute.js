1e66b6179c1a9c3886fcd0d4c51c4e41be02e746
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:53:19 +0545

    add the admin user route

--- backend/src/routes/admin/adminuserroute.js
+++ backend/src/routes/admin/adminuserroute.js
@@ -1,14 +1,11 @@
-const { getusers, deleteuser } = require("../../controllers/admin/users/usercontroller")
-const isAuthenticated = require("../../middlewares/authMiddleware")
-const restrictto = require("../../middlewares/restrict")
-const catchasync = require("../../services/catchasync")
+const {getusers, deleteuser} = require("../../controllers/admin/users/usercontroller");
+const isAuthenticated = require("../../middlewares/authMiddleware");
+const restrictto = require("../../middlewares/restrict");
+const catchasync = require("../../services/catchasync");
 
+const router = require("express").Router();
+router.route("/users").get(isAuthenticated, restrictto("admin"), catchasync(getusers));
 
-const router = require("express").Router()
- router.route("/users")
- .get(isAuthenticated,restrictto("admin"),catchasync(getusers))
+router.route("/users/:id").delete(isAuthenticated, restrictto("admin"), catchasync(deleteuser));
 
- router.route("/users/:id")
- .delete(isAuthenticated,restrictto("admin"),catchasync(deleteuser))
-
-module.exports =router
+module.exports = router;
