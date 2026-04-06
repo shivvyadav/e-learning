36fe230ac13bff65f9e31e92a153d9a3e71a2f3a
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:50:19 +0545

    add the email sender service

--- backend/src/services/sendemail.js
+++ backend/src/services/sendemail.js
@@ -1,19 +1,19 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
-const sendemail = async(options)=>{
-    var transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-          user:process.env.EMAIL_USER,
-          pass:process.env.EMAIL_PASS,  
-        }
-    })
-    const mailOptions={
-        from:`Sumit Rijal <${process.env.EMAIL_USER}>`,
-        to:options.email,
-        subject:options.subject,
-        text:options.message
-    };
-     await transporter.sendMail(mailOptions);
-}
+const sendemail = async (options) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const mailOptions = {
+    from: `Sumit Rijal <${process.env.EMAIL_USER}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(mailOptions);
+};
 module.exports = sendemail;
