80350eb77fe4a7e7be5684d7df9cc3b59604d40d
Shiv  Yadav <shivyadav0014@gmail.com>
2026-04-06 23:49:48 +0545

    update the catch async file

--- backend/src/services/catchasync.js
+++ backend/src/services/catchasync.js
@@ -1,14 +1,10 @@
-//this is a higher order function which takes another function as a argument
-//and here first it does simillar to try catch first if error donot occur it works
-//properly and if error occur then it catch them in catch((err))
-
-module.exports =(fn)=>{
-    return(req,res,next)=>{
-        fn(req,res,next).catch((err)=>{
-            return res.status(500).json({
-                message:err.message,
-                fullerror:err
-            })
-        })
-    }
-}
+module.exports = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => {
+      return res.status(500).json({
+        message: err.message,
+        fullerror: err,
+      });
+    });
+  };
+};
